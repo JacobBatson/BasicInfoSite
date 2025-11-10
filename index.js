615fd38a820c73651478ffecc09f9b73fd822491
JacobBatson <jacobbatson123@aol.com>
2025-11-10 18:03:51 -0500

    fixed up project with express

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
-const http = require('http');
-const fs = require('fs/promises');
+const express = require('express')
+const app = express();
 const path = require('path')
 
-const server = http.createServer(async (req,res) => { 
-    try {
-        let filePath;
-        if(req.url === '/') filePath = '/index.html'
-        if(req.url === '/contact') filePath = '/contact.html';
-        if(req.url === '/about') filePath = '/about.html';
+app.use((req, res) => {
+    let filePath;
 
-        const data = await fs.readFile(path.join(__dirname,filePath))
+    switch(req.path){
+        case '/':
+            filePath = 'index.html'
+            break;
+        case '/contact':
+            filePath = 'contact.html'
+            break;
+        case '/about':
+            filePath = 'about.html'
+            break;
+        default:
+            filePath = '404.html'
+            break;
 
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(data);
-    } catch(err){
-        res.writeHead(404, { 'Content-Type': 'text/html' });
-        res.end('<h1>404 Not Found</h1>')
     }
+
+    res.sendFile(path.join(__dirname,filePath))
 })
 
-server.listen(3000, () => {
-    console.log('server running')
+app.listen(3000, () => {
+    console.log("listening")
 })
